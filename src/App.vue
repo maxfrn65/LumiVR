63b176a2e4b4daab8ccc83d6e140b6b0d7f76066
maxime <m.fourna@icloud.com>
2024-03-15 15:19:53 +0100

    chore: added maintenance message

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+
+<template>
+  <h1>Ce site est en maintenance</h1>
+</template>
 
 <style>
 #app {
