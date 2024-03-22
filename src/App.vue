5cabfe65151f13bbc381529c2dced249736128db
maxime <m.fourna@icloud.com>
2024-03-22 17:10:15 +0100

    feat: added div layout + hero banner

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hero flex flex-col h-screen bg-[url('assets/hero.jpeg')] bg-cover">
+    <header class="h-28"></header>
+    <div class=" h-full"></div>
+  </div>
+  <div class="flex flex-col pt-24 gap-24">
+    <div class="flex columns-2 h-96 pl-48 pr-48 gap-8">
+      <div class="bg-blue-200 h-full w-2/3"></div>
+      <div class="bg-emerald-200 h-full w-1/3"></div>
+    </div>
+    <div>
+      <hr>
+    </div>
+    <footer class="bg-fuchsia-200 h-32"></footer>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
