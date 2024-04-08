03b9ef01e4f40e3b83ac61e49bcbeee32340d5cf
maxime <m.fourna@icloud.com>
2024-04-08 13:45:04 +0200

    feat: added aside nav link component

--- src/components/aside-nav-link.vue
+++ src/components/aside-nav-link.vue
@@ -1,11 +1,28 @@
 <script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: false,
+    default: "#"
+  }
+})
+
 
 </script>
 
 <template>
-
+  <h4><a class="focus:before:content-asterisk transition-all text-white/80 hover:text-white focus:text-white" :href="url">{{text}}</a></h4>
 </template>
 
 <style scoped>
-
+ a {
+   font-size: 1rem;
+   font-style: normal;
+   font-weight: 700;
+   line-height: normal;
+ }
 </style>
