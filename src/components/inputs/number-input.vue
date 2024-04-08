ab8c887d96eb10be7bbabec41295093aebafdf26
maxime <m.fourna@icloud.com>
2024-04-08 13:46:34 +0200

    feat: added number input component

---
+++ src/components/inputs/number-input.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <input class="bg-white/10 px-2.5 py-3 rounded-md border-2 border-white/10 placeholder-white/30 focus:outline-0" type="number" placeholder="0.00">
+</template>
+
+<style scoped>
+
+</style>
